0b020de0600c095a747f730d6c335299edaea910
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-18 17:03:17 +1000

    add validation in flat model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_065029) do
+ActiveRecord::Schema.define(version: 2020_06_18_070022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_06_18_065029) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
 end
